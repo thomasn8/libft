82e0a1daef52804453657c66cbeb86f86caf222a
Thomas Nanchen <thomasnanchen@hotmail.com>
2021-10-13 15:58:22 +0200

    message

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,27 +6,19 @@
 /*   By: tnanchen <thomasnanchen@hotmail.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/13 13:26:58 by tnanchen          #+#    #+#             */
-/*   Updated: 2021/10/13 13:29:11 by tnanchen         ###   ########.fr       */
+/*   Updated: 2021/10/13 15:52:45 by tnanchen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*
-The strlen() function calculates the length of the string pointed
-to by s, excluding the terminating null byte ('\0').
-
-The strlen() function returns the number of bytes in the string
-pointed to by s.
-*/
 #include "ft_strlen.h"
 
-size_t ft_strlen(const char *s)
+size_t	ft_strlen(const char *s)
 {
-	int		i;
-	size_t 	len;
+	size_t	len;
 
-	i = 0;
-	while (s[i])
-		i++;
-	len = i * sizeof(char);
+	len = 0;
+	while (s[len])
+		len++;
+	len *= sizeof(char);
 	return (len);
 }
--- main.c
+++ main.c
@@ -25,12 +25,16 @@
 #include "ft_strlen.h"
 
 // test function
-int		print_errors(int test_index, char *function_name, int ft_function, int c_function)
+static int error_count = 0;		//global variable to get total of errors at the end of the tests
+static int test_index = 1;
+int		print_errors(char *function_name, int ft_function, int c_function)
 {
+	test_index++;
 	if (ft_function != c_function)
 	{
-		printf("%d. ERROR in function \"%s\"\n", test_index, function_name);
+		printf("ERROR (test n.%d) in function \"%s\"\n", test_index, function_name);
 		printf("ft_function : %d\nC_function : %d\n\n", ft_function, c_function);
+		error_count++;
 		return(1);
 	}
 	return (0);
@@ -40,67 +44,72 @@ int		print_errors(int test_index, char *function_name, int ft_function, int c_fu
 int	main(void)
 {
 	// Makefile test : 
-	printf("Makefile okay\n");
+	printf("Makefile is okay\n\n");
 
 	// ft_isalpha
-	print_errors(1, "ft_isalpha",		ft_isalpha('c'),	isalpha('c'));
-	print_errors(2, "ft_isalpha",		ft_isalpha('?'),	isalpha('?'));
-	print_errors(3, "ft_isalpha",		ft_isalpha('Z'),	isalpha('Z'));
-	print_errors(4, "ft_isalpha",		ft_isalpha(2),		isalpha(2));
-	print_errors(5, "ft_isalpha",		ft_isalpha('9'),	isalpha('9'));
-	print_errors(6, "ft_isalpha",		ft_isalpha(99),		isalpha(99));
-	print_errors(7, "ft_isalpha",		ft_isalpha(' '),	isalpha(' '));
-	print_errors(9, "ft_isalpha",		ft_isalpha('\n'),	isalpha('\n'));
+	print_errors("ft_isalpha",		ft_isalpha('c'),	isalpha('c'));
+	print_errors("ft_isalpha",		ft_isalpha('?'),	isalpha('?'));
+	print_errors("ft_isalpha",		ft_isalpha('Z'),	isalpha('Z'));
+	print_errors("ft_isalpha",		ft_isalpha(2),		isalpha(2));
+	print_errors("ft_isalpha",		ft_isalpha('9'),	isalpha('9'));
+	print_errors("ft_isalpha",		ft_isalpha(99),		isalpha(99));
+	print_errors("ft_isalpha",		ft_isalpha(' '),	isalpha(' '));
+	print_errors("ft_isalpha",		ft_isalpha('\n'),	isalpha('\n'));
 
 	// ft_isdigit
-	print_errors(1, "ft_isdigit",		ft_isdigit('c'),	isdigit('c'));
-	print_errors(2, "ft_isdigit",		ft_isdigit('?'),	isdigit('?'));
-	print_errors(3, "ft_isdigit",		ft_isdigit(' '),	isdigit(' '));
-	print_errors(4, "ft_isdigit",		ft_isdigit(0),		isdigit(0));
-	print_errors(5, "ft_isdigit",		ft_isdigit('0'),	isdigit('0'));
-	print_errors(6, "ft_isdigit",		ft_isdigit(2),		isdigit(2));
-	print_errors(7, "ft_isdigit",		ft_isdigit('2'),	isdigit('2'));
-	print_errors(8, "ft_isdigit",		ft_isdigit(11),		isdigit(11));
-	print_errors(9, "ft_isdigit",		ft_isdigit('\n'),	isdigit('\n'));
+	print_errors("ft_isdigit",		ft_isdigit('c'),	isdigit('c'));
+	print_errors("ft_isdigit",		ft_isdigit('?'),	isdigit('?'));
+	print_errors("ft_isdigit",		ft_isdigit(' '),	isdigit(' '));
+	print_errors("ft_isdigit",		ft_isdigit(0),		isdigit(0));
+	print_errors("ft_isdigit",		ft_isdigit('0'),	isdigit('0'));
+	print_errors("ft_isdigit",		ft_isdigit(2),		isdigit(2));
+	print_errors("ft_isdigit",		ft_isdigit('2'),	isdigit('2'));
+	print_errors("ft_isdigit",		ft_isdigit(11),		isdigit(11));
+	print_errors("ft_isdigit",		ft_isdigit('\n'),	isdigit('\n'));
 
 	// ft_isalnum
-	print_errors(1, "ft_isalnum",		ft_isalnum('c'),	isalnum('c'));
-	print_errors(2, "ft_isalnum",		ft_isalnum('?'),	isalnum('?'));
-	print_errors(3, "ft_isalnum",		ft_isalnum(' '),	isalnum(' '));
-	print_errors(4, "ft_isalnum",		ft_isalnum(0),		isalnum(0));
-	print_errors(5, "ft_isalnum",		ft_isalnum(11),		isalnum(11));
-	print_errors(6, "ft_isalnum",		ft_isalnum('\n'),	isalnum('\n'));
+	print_errors("ft_isalnum",		ft_isalnum('c'),	isalnum('c'));
+	print_errors("ft_isalnum",		ft_isalnum('?'),	isalnum('?'));
+	print_errors("ft_isalnum",		ft_isalnum(' '),	isalnum(' '));
+	print_errors("ft_isalnum",		ft_isalnum(0),		isalnum(0));
+	print_errors("ft_isalnum",		ft_isalnum(11),		isalnum(11));
+	print_errors("ft_isalnum",		ft_isalnum('\n'),	isalnum('\n'));
 
 	// ft_isascii
-	print_errors(1, "ft_isascii",		ft_isascii('c'),	isascii('c'));
-	print_errors(2, "ft_isascii",		ft_isascii('?'),	isascii('?'));
-	print_errors(3, "ft_isascii",		ft_isascii(' '),	isascii(' '));
-	print_errors(4, "ft_isascii",		ft_isascii(0),		isascii(0));
-	print_errors(5, "ft_isascii",		ft_isascii(11),		isascii(11));
-	print_errors(6, "ft_isascii",		ft_isascii('\n'),	isascii('\n'));
-	print_errors(7, "ft_isascii",		ft_isascii(0x7c),	isascii(0x7c));
-	print_errors(8, "ft_isascii",		ft_isascii(0x80),	isascii(0x80));
-	print_errors(9, "ft_isascii",		ft_isascii(0x81),	isascii(0x81));
+	print_errors("ft_isascii",		ft_isascii('c'),	isascii('c'));
+	print_errors("ft_isascii",		ft_isascii('?'),	isascii('?'));
+	print_errors("ft_isascii",		ft_isascii(' '),	isascii(' '));
+	print_errors("ft_isascii",		ft_isascii(0),		isascii(0));
+	print_errors("ft_isascii",		ft_isascii(11),		isascii(11));
+	print_errors("ft_isascii",		ft_isascii('\n'),	isascii('\n'));
+	print_errors("ft_isascii",		ft_isascii(0x7c),	isascii(0x7c));
+	print_errors("ft_isascii",		ft_isascii(0x80),	isascii(0x80));
+	print_errors("ft_isascii",		ft_isascii(0x81),	isascii(0x81));
 	// printf("%d\n", ft_isascii(0x7c)); printf("%d\n", isascii(0x7c));
 	// printf("%d\n", ft_isascii(0x80)); printf("%d\n", isascii(0x80));
 	// printf("%d\n", ft_isascii(0x81)); printf("%d\n", isascii(0x81));
 
 	// ft_isprint
-	print_errors(1, "ft_isprint",		ft_isprint('c'),	isprint('c'));
-	print_errors(2, "ft_isprint",		ft_isprint('\t'),	isprint('\t'));
-	print_errors(3, "ft_isprint",		ft_isprint('Z'),	isprint('Z'));
-	print_errors(4, "ft_isprint",		ft_isprint(2),		isprint(2));
-	print_errors(5, "ft_isprint",		ft_isprint('9'),	isprint('9'));
-	print_errors(6, "ft_isprint",		ft_isprint(99),		isprint(99));
-	print_errors(7, "ft_isprint",		ft_isprint(' '),	isprint(' '));
-	print_errors(9, "ft_isprint",		ft_isprint('\n'),	isprint('\n'));
-	print_errors(10, "ft_isprint",		ft_isprint(0x7c),	isprint(0x7c));
-	print_errors(11, "ft_isprint",		ft_isprint(0x80),	isprint(0x80));
-	print_errors(12, "ft_isprint",		ft_isprint(0),		isprint(0));
+	print_errors("ft_isprint",		ft_isprint('c'),	isprint('c'));
+	print_errors("ft_isprint",		ft_isprint('\t'),	isprint('\t'));
+	print_errors("ft_isprint",		ft_isprint('Z'),	isprint('Z'));
+	print_errors("ft_isprint",		ft_isprint(2),		isprint(2));
+	print_errors("ft_isprint",		ft_isprint('9'),	isprint('9'));
+	print_errors("ft_isprint",		ft_isprint(99),		isprint(99));
+	print_errors("ft_isprint",		ft_isprint(' '),	isprint(' '));
+	print_errors("ft_isprint",		ft_isprint('\n'),	isprint('\n'));
+	print_errors("ft_isprint",		ft_isprint(0x7c),	isprint(0x7c));
+	print_errors("ft_isprint",		ft_isprint(0x80),	isprint(0x80));
+	print_errors("ft_isprint",		ft_isprint(0),		isprint(0));
 
-	// ft_strlen()
-	// print_errors(1, "ft_strlen",		ft_strlen('salut'),	ft_strlen('salut'));
-	printf("%lu\n", ft_strlen("salut")); printf("%lu\n", strlen("salut"));
-	
+	// ft_strlen
+	print_errors("ft_strlen",		ft_strlen("salut"),	ft_strlen("salut"));
+	print_errors("ft_strlen",		ft_strlen("salut lausanne"),	ft_strlen("salut lausanne"));
+	char *lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n";
+	print_errors("ft_strlen",		ft_strlen(lorem),	ft_strlen(lorem));
+	// printf("%lu\n", ft_strlen("salut lausanne")); printf("%lu\n", strlen("salut lausanne"));
+	// printf("%lu\n", ft_strlen(lorem)); printf("%lu\n", strlen(lorem));
+
+	printf("%d errors in tests.\n", error_count);
 	return (0);
 }
