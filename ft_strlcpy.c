1b0414adeedb9c62d0a36bd869e26b376e5df04b
Thomas Nanchen <thomasnanchen@hotmail.com>
2021-10-19 20:38:25 +0200

    message

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,37 +6,29 @@
 /*   By: tnanchen <thomasnanchen@hotmail.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/14 16:14:12 by tnanchen          #+#    #+#             */
-/*   Updated: 2021/10/19 20:27:50 by tnanchen         ###   ########.fr       */
+/*   Updated: 2021/10/19 20:37:53 by tnanchen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libc.h"
 
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
 	size_t	i;
-	size_t	src_length;
 
-	src_length = 0;
-	while (src[src_length])
-		src_length++;
-	i = src_length;
-	if (dstsize)
-		dstsize--;
-	dst[dstsize] = 0;
-	while (i-- && dstsize--)
-		*dst++ = *src++;
-	return (src_length);
+	i = 0;
+	if (!dst || !src)
+		return (0);
+	if (size > 0)
+	{
+		while (--size && src[i])
+		{
+			dst[i] = src[i];
+			i++;
+		}
+		dst[i] = 0;
+	}
+	while (src[i])
+		i++;
+	return (i);
 }
-
-/*
-strlcpy() take the full size of the destination buffer and
-guarantee NUL-termination if there is room.  
-Note that room for the NUL should be included in dstsize.
-
-strlcpy() copies up to dstsize - 1 characters from the string src to dst,
-NUL-terminating the result if dstsize is not 0.
-
-strlcpy() return the total length of the string it tried to create.  
-For strlcpy() that means the length of src.
-*/
