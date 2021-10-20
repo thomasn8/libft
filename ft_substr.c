38000c82cd9d79d041707f20939e774887c6123f
tnanchen <thomasnanchen@hotmail.com>
2021-10-20 13:05:28 +0200

    message

--- ft_substr.c
+++ ft_substr.c
@@ -6,13 +6,12 @@
 /*   By: tnanchen <thomasnanchen@hotmail.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/17 22:14:28 by tnanchen          #+#    #+#             */
-/*   Updated: 2021/10/17 23:14:23 by tnanchen         ###   ########.fr       */
+/*   Updated: 2021/10/20 13:01:10 by tnanchen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libc.h"
 
-// KO.3 KO.9
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*substr;
@@ -24,37 +23,18 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	while (s[i])
 	{
 		if (i >= start && j < len)
-			j++;					// taille du malloc
+			j++;
 		i++;
 	}
 	substr = malloc(j * sizeof(char) + 1);
 	if (!substr)
 		return (NULL);
 	substr[j] = 0;
-	while (j--)
-		substr[j] = s[j];
+	i = 0;
+	while (i < j)
+	{
+		substr[i] = s[start + i];
+		i++;
+	}
 	return (substr);
 }
-
-// KO.5
-// char	*ft_substr(char const *s, unsigned int start, size_t len)
-// {
-// 	char	*substr;
-// 	size_t	i;
-// 	size_t	s_len;
-// 	
-// 
-// 
-// 	s_len = start;
-// 	while(s[s_len] && len--)
-// 		s_len++;
-// 	s_len -= start;
-// 	substr = malloc(s_len * sizeof(char) + 1);
-// 	if (!substr)
-// 		return (NULL);
-// 	i = -1;
-// 	while (++i < s_len)
-// 		substr[i] = s[start + i];
-// 	substr[i] = 0;
-// 	return (substr);
-// }
