4886d3d1d00bc71693ccac03f84a3d1364e82bff
Thomas Nanchen <thomasnanchen@hotmail.com>
2021-10-20 23:45:25 +0200

    message

--- ft_split.c
+++ ft_split.c
@@ -29,14 +29,24 @@ static int	words_count(char const *s, char c)
 		return (1);
 	while (s[i] || (i == 0))
 	{
+		printf("Start 1st while at [%d]\n", i);
 		while (s[i] == c)
-			++i;
-		start = i;
-		while (s[i] != c && s[i])
+		{
 			i++;
+		}
+		start = i;
+		// printf("First letter (%d of s) of word %d : %c\n", start, words, s[start]);
+		while (s[i] != c && s[i])
+		{
+			i++;
+		}
 		end = i;
-		words++;
+		// printf("Last letter (%d of s) of word %d : %c\n", end, words, s[end-1]);
+		if (s[start] != 0)		
+			words++;
+		// printf("%d words\n", words);
 	}
+	printf("Nombre final de words : %d\n", words);
 	return (words);
 }
 
@@ -52,15 +62,24 @@ static int	words_alloc(char **tab, char const *s, char c, int i)
 	while (s[i] || (i == 0))
 	{
 		while (s[i] == c)
-			++i;
+			i++;
 		start = i;
 		while (s[i] != c && s[i])
 			i++;
 		end = i;
 		tab[word] = malloc((end - start) * sizeof(char) + 1);
+		printf("Malloc size (%d): %d\n", word, (end - start + 1));
 		if (!tab)
 			return (0);
 		word++;
+	}
+	// printf("start : %d / %c\n", start, s[start]);
+	// printf("end : %d/%c\n", end, s[end]);
+	// printf("i : %d/%c\n", i, s[i]);
+	if (start != end)
+	{
+		tab[word] = malloc(1 * sizeof(char));
+		printf("Malloc size (%d): %d\n", word, 1);
 	}
 	return (1);
 }
@@ -79,7 +98,7 @@ static void	words_split(char **tab, char const *s, char c, int i)
 	while (s[i] || (i == 0))
 	{
 		while (s[i] == c)
-			++i;
+			i++;
 		start = i;
 		while (s[i] != c && s[i])
 			i++;
@@ -87,6 +106,7 @@ static void	words_split(char **tab, char const *s, char c, int i)
 		while (start < end)
 			tab[word][letter++] = s[start++];
 		tab[word][letter] = 0;
+		printf("word %d : %s\n", word, tab[word]);
 		word++;
 		letter = 0;
 	}
@@ -105,9 +125,31 @@ char	**ft_split(char const *s, char c)
 	tab = malloc(words * sizeof(char *) + 1);
 	if (!tab)
 		return (NULL);
+	printf("Tab malloc size pour %d mot(s) : %lu\n",words+1, (words * sizeof(char *) + 1));
 	tab[words] = NULL;
+	printf("tab : %p\n", tab);
+	printf("tab[0] : %p | Val : ", tab[0]);
+	printf("%s\n", tab[0]);
+	printf("tab[1] : %p | Val : ", tab[1]);
+	printf("%s\n", tab[1]);
+	printf("tab[2] : %p | Val : ", tab[2]);
+	printf("%s\n", tab[2]);
 	if ((words_alloc(tab, s, c, i)) == 0)
 		return (NULL);
+	printf("tab : %p\n", tab);
+	printf("tab[0] : %p | Val : ", tab[0]);
+	printf("%s\n", tab[0]);
+	printf("tab[1] : %p | Val : ", tab[1]);
+	printf("%s\n", tab[1]);
+	printf("tab[2] : %p | Val : ", tab[2]);
+	printf("%s\n", tab[2]);
 	words_split(tab, s, c, i);
+	printf("tab : %p\n", tab);
+	printf("tab[0] : %p | Val : ", tab[0]);
+	printf("%s\n", tab[0]);
+	printf("tab[1] : %p | Val : ", tab[1]);
+	printf("%s\n", tab[1]);
+	printf("tab[2] : %p | Val : ", tab[2]);
+	printf("%s\n", tab[2]);
 	return (tab);
 }
--- libft_tests/main.c
+++ libft_tests/main.c
@@ -1097,94 +1097,116 @@ int	main(void)
 	// /* 8 */ mcheck(s, 1); free(s); showLeaks();
 
 	// ft_split
-	char *s1 = "_Une__petite_chaineeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee____de_caracteres.\0";
-	char c1 = '_';
-	char **words;
-	words = ft_split(s1, c1);
-	// Expected words :
-	char *word_1_1 = "Une";
-	char *word_1_2 = "petite";
-	char *word_1_3 = "chaineeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee";
-	char *word_1_4 = "de";
-	char *word_1_5 = "caracteres.";
-	// Tests :
-	print_errors_string("ft_split", ft_split(s1, c1)[0], word_1_1);
-	print_errors_string("ft_split", ft_split(s1, c1)[1], word_1_2);
-	print_errors_string("ft_split", ft_split(s1, c1)[2], word_1_3);
-	print_errors_string("ft_split", ft_split(s1, c1)[3], word_1_4);
-	print_errors_string("ft_split", ft_split(s1, c1)[4], word_1_5);
-	//
-	//
-	char *s2 = "     salut       les   copines    \0 Vous allez bien ?\0";
-	char c2 = ' ';
-	char **words2;
-	words2 = ft_split(s2, c2);
-	// Expected words :
-	char *word_2_1 = "salut";
-	char *word_2_2 = "les";
-	char *word_2_3 = "copines";
-	// Tests :
-	print_errors_string("ft_split", ft_split(s2, c2)[0], word_2_1);
-	print_errors_string("ft_split", ft_split(s2, c2)[1], word_2_2);
-	print_errors_string("ft_split", ft_split(s2, c2)[2], word_2_3);
-	//
-	//
-	char *s3 = "_Une__petite_chaineeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee____de_caracteres.\0";
-	char c3 = 0;
-	char **words3;
-	words3 = ft_split(s3, c3);
-	// Expected words :
-	char *word_3_1 = "_Une__petite_chaineeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee____de_caracteres.";
-	// Tests :
-	print_errors_string("ft_split", ft_split(s3, c3)[0], word_3_1);
-	//
-	//
-	char *s4 = 0;
-	char c4 = ' ';
-	// char c4 = 0;
-	char **words4;
-	words4 = ft_split(s4, c4);
-	// Tests :
-	test_index++;
-	if(words4 != NULL)
-	{
-		red_color();
-		printf("ERROR (test n.%d) in function \"ft_split\"\n", test_index);
-		defaut_color();
-		printf("ft_function : %p | Expected :  %s\n\n", words4, NULL);
-		error_count++;
-		return(1);
-	}
-	//
-	//
-	char *s5 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\0";
-	char c5 = ' ';
-	char c6 = 'e';
-	char **words5;
-	char **words6;
-	words5 = ft_split(s5, c5);
-	words6 = ft_split(s5, c6);
+	// char *s1 = "_Une__petite_chaineeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee____de_caracteres.\0";
+	// char c1 = '_';
+	// char **words;
+	// words = ft_split(s1, c1);
+	// // Expected words :
+	// char *word_1_1 = "Une";
+	// char *word_1_2 = "petite";
+	// char *word_1_3 = "chaineeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee";
+	// char *word_1_4 = "de";
+	// char *word_1_5 = "caracteres.";
+	// // Tests :
+	// print_errors_string("ft_split", ft_split(s1, c1)[0], word_1_1);
+	// print_errors_string("ft_split", ft_split(s1, c1)[1], word_1_2);
+	// print_errors_string("ft_split", ft_split(s1, c1)[2], word_1_3);
+	// print_errors_string("ft_split", ft_split(s1, c1)[3], word_1_4);
+	// print_errors_string("ft_split", ft_split(s1, c1)[4], word_1_5);
+	// //
+	// //
+	// char *s2 = "     salut       les   copines    \0 Vous allez bien ?\0";
+	// char c2 = ' ';
+	// char **words2;
+	// words2 = ft_split(s2, c2);
+	// // Expected words :
+	// char *word_2_1 = "salut";
+	// char *word_2_2 = "les";
+	// char *word_2_3 = "copines";
+	// // Tests :
+	// print_errors_string("ft_split", ft_split(s2, c2)[0], word_2_1);
+	// print_errors_string("ft_split", ft_split(s2, c2)[1], word_2_2);
+	// print_errors_string("ft_split", ft_split(s2, c2)[2], word_2_3);
+	// //
+	// //
+	// char *s3 = "_Une__petite_chaineeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee____de_caracteres.\0";
+	// char c3 = 0;
+	// char **words3;
+	// words3 = ft_split(s3, c3);
+	// // Expected words :
+	// char *word_3_1 = "_Une__petite_chaineeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee____de_caracteres.";
+	// // Tests :
+	// print_errors_string("ft_split", ft_split(s3, c3)[0], word_3_1);
+	// //
+	// //
+	// char *s4 = 0;
+	// char c4 = ' ';
+	// // char c4 = 0;
+	// char **words4;
+	// words4 = ft_split(s4, c4);
+	// // Tests :
+	// test_index++;
+	// if(words4 != NULL)
+	// {
+	// 	red_color();
+	// 	printf("ERROR (test n.%d) in function \"ft_split\"\n", test_index);
+	// 	defaut_color();
+	// 	printf("ft_function : %p | Expected :  %s\n\n", words4, NULL);
+	// 	error_count++;
+	// 	return(1);
+	// }
+	// //
+	// //
+	// char *s5 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\0";
+	// char c5 = ' ';
+	// char c6 = 'e';
+	// char **words5;
+	// char **words6;
+	// words5 = ft_split(s5, c5);
+	// words6 = ft_split(s5, c6);
 
 
-	int words5_count = 0;
-	int words5_i = -1;
-	while(words5[++words5_i] != NULL)
-		words5_count++;
-	// printf("words5_count : %d\n", words5_count);
-	// printf("words5[137] : %s\n", words5[137]);
-	// printf("words5[138] : %s\n", words5[138]);
-	print_errors_string("ft_split", words5[0], "Lorem");
-	print_errors_string("ft_split", words5[137], "laborum.");
+	// int words5_count = 0;
+	// int words5_i = -1;
+	// while(words5[++words5_i] != NULL)
+	// 	words5_count++;
+	// // printf("words5_count : %d\n", words5_count);
+	// // printf("words5[137] : %s\n", words5[137]);
+	// // printf("words5[138] : %s\n", words5[138]);
+	// print_errors_string("ft_split", words5[0], "Lorem");
+	// print_errors_string("ft_split", words5[137], "laborum.");
 
-	int words6_count = 0;
-	int words6_i = -1;
-	while(words6[++words6_i] != NULL)
-		words6_count++;
-	// printf("words6_count : %d\n", words6_count);
-	// printf("words6[74] : %s\n", words6[74]);
-	// printf("words6[75] : %s\n", words6[75]);
-	print_errors_string("ft_split", words6[0], "Lor");
-	print_errors_string("ft_split", words6[74], "st laborum.");
+	// int words6_count = 0;
+	// int words6_i = -1;
+	// while(words6[++words6_i] != NULL)
+	// 	words6_count++;
+	// // printf("words6_count : %d\n", words6_count);
+	// // printf("words6[74] : %s\n", words6[74]);
+	// // printf("words6[75] : %s\n", words6[75]);
+	// print_errors_string("ft_split", words6[0], "Lor");
+	// print_errors_string("ft_split", words6[74], "st laborum.");
+
+	// SPLIT TRIPOUILLE TESTS
+	char **tab = ft_split("  tripouille  42     ", ' ');
+	// while(tab)
+	// 	printf("%s\n", *tab++);
+
+	// printf("\n%s\n", tab[0]);
+	// printf("%s\n", tab[1]);
+	// printf("%s\n", tab[2]);
+	focus_on_backslash_zero(tab[2], 15);
+	
+	// /* 1 */ mcheck(tab, sizeof(char *) * 3);
+
+	// /* 2 */ check(!strcmp(tab[0], "tripouille"));
+	// /* 3 */ mcheck(tab[0], strlen("tripouille") + 1);
+
+	// /* 4 */ check(!strcmp(tab[1], "42"));
+	// /* 5 */ mcheck(tab[1], strlen("42") + 1);
+
+	// /* 6 */ check(tab[2] == NULL);
+	// freeTab(tab); showLeaks();
+
 
 	// ft_itoa 
 	int number1 = 0;
